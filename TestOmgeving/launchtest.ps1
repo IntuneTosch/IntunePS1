b672bfe72a1050d4a147c3ad5323741bacb18271
IntuneTosch <mv@tosch.nl>
2025-05-28 09:22:30 +0200

    Update launchtest.ps1

--- TestOmgeving/launchtest.ps1
+++ TestOmgeving/launchtest.ps1
@@ -41,7 +41,7 @@ if (-not (Test-Path $iconPath)) {
 $XAML = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="Tosch Intune 1.6" Height="390" Width="600" Background="#f5f1e9"
+        Title="Tosch Intune Test 1.7" Height="390" Width="600" Background="#f5f1e9"
         WindowStartupLocation="CenterScreen">
     <Grid Margin="10">
         <Grid.ColumnDefinitions>
