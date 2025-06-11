4027e1313239e9bf1303a14e490deeabcd50c850
IntuneTosch <mv@tosch.nl>
2025-06-11 15:24:28 +0200

    Change Update to 1.7.3

--- Production/ISOZonderProfiel/mainnp_en.ps1
+++ Production/ISOZonderProfiel/mainnp_en.ps1
@@ -3,7 +3,7 @@
 ###############################################################################################################
 
 #Script Version
-$Scriptversion = "1.8.0"
+$Scriptversion = "1.7.3"
 
 # Define default ISO file path
 $DefaultISOPath1 = "C:\Users\$env:USERNAME\Tosch Automatisering B.V\Techniek - General\ISO\Windows 11 Intune\W11IntuneEnglish1.7.iso"
